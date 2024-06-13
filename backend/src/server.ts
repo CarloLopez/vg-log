bedafccc932523e659c4d7b6e7281c7b4241ef27
CarloLopez <86852232+CarloLopez@users.noreply.github.com>
2024-06-13 12:20:19 +0100

    Move temp Data

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,25 +4,27 @@ import cors from 'cors';
 import path from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Interface for the request body
-interface ApiRequestBody {
+// types
+type ApiRequestBody = {
   endpoint: string;
   body: string;
 }
 
-// Routes
+// load env variables
+dotenv.config();
+
+// create server
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// routes
 app.post('/api/games', async (req: Request, res: Response) => {
   const { endpoint, body }: ApiRequestBody = req.body;
 
@@ -42,12 +44,12 @@ app.post('/api/games', async (req: Request, res: Response) => {
   }
 });
 
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+// fallback handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-// Start the server
+// start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
